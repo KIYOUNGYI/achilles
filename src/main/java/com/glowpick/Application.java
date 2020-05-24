eb83f2990bb5f3407b3199ba46a80fd8a25983f6
liki <>
2020-05-24 23:29:42 +0900

    hibernate, h2 study

--- src/main/java/com/glowpick/Application.java
+++ src/main/java/com/glowpick/Application.java
@@ -3,12 +3,20 @@ package com.glowpick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @SpringBootApplication
 public class Application
 {
     public static void main(String[] args)
     {
         SpringApplication app = new SpringApplication(Application.class);
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.close();
+        entityManagerFactory.close();
         app.run(args);
     }
 }
