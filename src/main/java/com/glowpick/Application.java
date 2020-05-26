fd0b4557ceecd265540cc405df76cdc10720d59f
yky1990@glowmee.com <yky1990@glowmee.com>
2020-05-26 10:49:00 +0900

    - flush 까지 강의 들음

--- src/main/java/com/glowpick/Application.java
+++ src/main/java/com/glowpick/Application.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 @SpringBootApplication
 public class Application
@@ -23,7 +24,8 @@ public class Application
 
 //        createDummy(entityManager, entityTransaction, entityManagerFactory);
 //        retrieveDummy(entityManager, entityTransaction, entityManagerFactory);
-        updateDummy(entityManager, entityTransaction, entityManagerFactory);
+//        updateDummy(entityManager, entityTransaction, entityManagerFactory);
+//        retrieveDummyByJpql(entityManager, entityTransaction, entityManagerFactory);
         app.run(args);
     }
 
@@ -50,6 +52,29 @@ public class Application
     }
 
 
+    private static void retrieveDummyByJpql(EntityManager entityManager, EntityTransaction entityTransaction, EntityManagerFactory entityManagerFactory)
+    {
+        entityTransaction.begin();
+        try
+        {
+            List<Member> result = entityManager.createQuery("select m from Member as m",Member.class).getResultList();
+
+            for(Member member:result)
+            {
+                System.out.println("member.name =  "+member.getName());
+            }
+
+        }
+        catch (Exception e)
+        {
+            entityTransaction.rollback();
+        }
+        finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+
     private static void retrieveDummy(EntityManager entityManager, EntityTransaction entityTransaction, EntityManagerFactory entityManagerFactory)
     {
         entityTransaction.begin();
@@ -74,10 +99,18 @@ public class Application
         // 정석이지만 이걸 스프링에서 해줌
         try
         {
+            // 비영속
             Member member = new Member();
             member.setId(2l);
             member.setName("Hello2");
+
+            // 영속
             entityManager.persist(member);
+            // 준영속 (엔티티매니저에서 떼어버리는 것)
+//            entityManager.detach(member);
+            // 삭제
+//            entityManager.remove(member);
+//            entityManager.flush();//db 반영
             entityTransaction.commit();
         }
         catch (Exception e)
